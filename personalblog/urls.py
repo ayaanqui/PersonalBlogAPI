cc9f92c937f42535c186a49c4d803069afc42b35
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-03-23 11:40:18 -0500

    Supported image urls

--- personalblog/urls.py
+++ personalblog/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/tags/', include('tags.api.urls'), name='tags'),
-    path('api/posts/', include('posts.api.urls'), name='posts')
+    path('api/posts/', include('posts.api.urls'), name='posts'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
